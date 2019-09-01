7230416cb735e4008f5ba3e4ae2d5bb9e3813b9c
“joaoviana” <joaopadraoviana@gmail.com>
2019-09-01 18:08:08 +0100

    added regex to check if its yes or no

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
-const goToSection = ((toSection, fromSection) => {
-    let toSectionElem = document.getElementById(`section${toSection}`);
-    let fromSectionElem = document.getElementById(`section${fromSection}`);
-    toSectionElem.classList.remove("section--disabled");
-    toSectionElem.classList.add("section--active");
+const scrollToTop = (() => {
+    const c = document.documentElement.scrollTop || document.body.scrollTop;
+    if (c > 0) {
+      window.requestAnimationFrame(scrollToTop);
+      window.scrollTo(0, c - c / 16);
+    }
+});
+
+const checkInput = (() => {
+    let answer = document.getElementById('answer').value;
+    let yesRegex = /y.?|sure.?/gi;
+    let noRegex = /n.?|hell no.?/gi;
+    console.log(yesRegex.test(answer))
 });
